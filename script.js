7f79a66408d8c9835642df285ee32c0704db4c18
Munitio <maier.felix96@gmail.com>
2015-02-15 13:47:07 +0100

    8bit square compression works

--- script.js
+++ script.js
@@ -5,12 +5,21 @@
     var JS2PNG = JS2PNG || {};
 
     var OUTPUT_FILTERED = 0,
-        OUTPUT_8BIT = 1,
-        OUTPUT_8BIT_GRAY = 1,
-        OUTPUT_24BIT = 1,
-        OUTPUT_32BIT = 1;
+        OUTPUT_8BIT = 1;
 
     var BYTE_STORE = {};
+    
+    /**
+     * Convert decimal to hex color
+     */
+    JS2PNG.DEC2HEX = function(number) {
+        if (number < 0) {
+            number = 0xFFFFFFFF + number + 1;
+        }
+        var result = number.toString(16).toUpperCase();
+
+        return "#" + result + result + result;
+    };
 
     JS2PNG.Encode = function() {
 
@@ -23,36 +32,123 @@
         }
 
         BYTE_STORE[prefix + "_ascii"] = bytes;
+        
+        BYTE_STORE.init("_ascii", data, prefix);
 
         bytes = [];
 
-        var sequences = Math.ceil((data.length) / 8); 
-
-        var cObj = {
-            c1: "",
-            c2: "",
-            c3: "",
-            c4: "",
-            c5: "",
-            c6: "",
-            c7: "",
-            c8: ""
+        var sequences = Math.ceil((data.length) / 8);
+        
+        var byteObject = {
+            c1: [],
+            c2: [],
+            c3: [],
+            c4: [],
+            c5: [],
+            c6: [],
+            c7: [],
+            c8: []
         };
 
         for (var ii = 0; ii < sequences; ++ii) {
-            cObj.c1 += data.substr( (ii * 8) + 0, 1);
-            cObj.c2 += data.substr( (ii * 8) + 1, 1);
-            cObj.c3 += data.substr( (ii * 8) + 2, 1);
-            cObj.c4 += data.substr( (ii * 8) + 3, 1);
-            cObj.c5 += data.substr( (ii * 8) + 4, 1);
-            cObj.c6 += data.substr( (ii * 8) + 5, 1);
-            cObj.c7 += data.substr( (ii * 8) + 6, 1);
-            cObj.c8 += data.substr( (ii * 8) + 7, 1);
+            byteObject.c1.push(data.substr( (ii * 8) + 0, 1).charCodeAt(0));
+            byteObject.c2.push(data.substr( (ii * 8) + 1, 1).charCodeAt(0));
+            byteObject.c3.push(data.substr( (ii * 8) + 2, 1).charCodeAt(0));
+            byteObject.c4.push(data.substr( (ii * 8) + 3, 1).charCodeAt(0));
+            byteObject.c5.push(data.substr( (ii * 8) + 4, 1).charCodeAt(0));
+            byteObject.c6.push(data.substr( (ii * 8) + 5, 1).charCodeAt(0));
+            byteObject.c7.push(data.substr( (ii * 8) + 6, 1).charCodeAt(0));
+            byteObject.c8.push(data.substr( (ii * 8) + 7, 1).charCodeAt(0));
+        }
+
+        /**
+         * Validate byte object array
+         * Convert byte object array
+         */
+        for (var ii in byteObject) {
+            for (var kk = 0; kk < byteObject[ii].length; ++kk) {
+                byteObject[ii][kk] = !isNaN(byteObject[ii][kk]) ? byteObject[ii][kk] : 0;
+            }
+        }
+
+        for (var ii = 0, resultArray = []; ii < sequences; ++ii) {
+            byteObject.b1 = (( byteObject.c1[ii] << 1) | ( byteObject.c2[ii] >> 6 )) & 0xff;
+            byteObject.b2 = (( byteObject.c2[ii] << 2) | ( byteObject.c3[ii] >> 5 )) & 0xff;
+            byteObject.b3 = (( byteObject.c3[ii] << 3) | ( byteObject.c4[ii] >> 4 )) & 0xff;
+            byteObject.b4 = (( byteObject.c4[ii] << 4) | ( byteObject.c5[ii] >> 3 )) & 0xff;
+            byteObject.b5 = (( byteObject.c5[ii] << 5) | ( byteObject.c6[ii] >> 2 )) & 0xff;
+            byteObject.b6 = (( byteObject.c6[ii] << 6) | ( byteObject.c7[ii] >> 1 )) & 0xff;
+            byteObject.b7 = (((byteObject.c7[ii] << 7) & 0x80) | (byteObject.c8[ii] & 0x7f)) & 0xff;
+            resultArray.push(byteObject.b1, byteObject.b2, byteObject.b3, byteObject.b4, byteObject.b5, byteObject.b6, byteObject.b7);
+        }
+
+        BYTE_STORE[prefix + "_seq8"] = resultArray;
+        
+        BYTE_STORE.init("_seq8", data, prefix);
+
+    };
+    
+    BYTE_STORE.init = function() {
+
+        this.mode = arguments[0];
+        this.data = arguments[1];
+        this.prefix = arguments[2];
+        this.size = this.data.length;
+        
+        var w = Math.floor(Math.sqrt(this.size)),
+            h = Math.ceil(this.size / w);
+
+        if (OUTPUT_8BIT) {
+            this.create_8b(this.mode, 'square', w, h);
         }
 
     };
+    
+    BYTE_STORE.create_8b = function() {
 
-    JS2PNG.Encode("HelloWorld", "Filename");
+        var mode = arguments[0],
+            type = arguments[1],
+            w = arguments[2],
+            h = arguments[3];
+
+        var canvas = document.createElement('canvas');
+            canvas.width = w;
+            canvas.height = h;
+
+        var ctx = canvas.getContext('2d');
+        var imageData = ctx.createImageData(w, h);
+
+        var cols = [];
+        for (var ii = 0; ii < 256; ++ii) {
+            cols[ii] = ii;
+        }
+
+        var data = this[this.prefix + mode];
+
+        var ii = 0;
+        for (var y = 0; y < h; ++y) {
+            for (var x = 0; x < w; ++x) {
+                
+                var b1 = parseInt(data[ii]);
+
+                var col = cols[b1] ? cols[b1] : 0;
+
+                col = JS2PNG.DEC2HEX(col);
+
+                ctx.fillStyle = col;
+                ctx.fillRect(x, y, 1, 1);
+
+                ii++;
+            }
+        }
+        
+        window.addEventListener('DOMContentLoaded', function() {
+            document.body.appendChild(canvas);
+        });
+            
+    };
+
+    JS2PNG.Encode("HelloWorldMyNameIsFelix", "Filename");
 
     root.JS2PNG = JS2PNG;
 
